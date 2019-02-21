00f87956bda63f1981b5d705047d64765fb131cf
CofCool <cofcool@126.com>
2019-02-21 20:02:53 +0800

    ExecuteResult 添加 apply 方法

--- common/src/main/java/net/cofcool/chaos/server/common/core/ExecuteResult.java
+++ common/src/main/java/net/cofcool/chaos/server/common/core/ExecuteResult.java
@@ -1,5 +1,8 @@
 package net.cofcool.chaos.server.common.core;
 
+import java.util.function.Function;
+import jdk.internal.jline.internal.Nullable;
+
 /**
  * 执行结果
  *
@@ -25,6 +28,22 @@ public interface ExecuteResult<T> extends Result<T> {
     @Override
     default boolean successful() {
         return getState() >= EXECUTE_STATE_SUCCESSFUL;
+    }
+
+    /**
+     * 如果 {@link #successful()} 为 true，则执行 fn，否则返回 null
+     *
+     * @param fn 需要执行的 Function
+     * @param <R> 返回数据类型
+     * @return fn 执行结果
+     */
+    @Nullable
+    default <R> R apply(Function<T, R> fn) {
+        if (successful()) {
+            return fn.apply(getEntity());
+        }
+
+        return null;
     }
 
 }
