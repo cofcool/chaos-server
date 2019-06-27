12f966cf0ef86052da7d59a6eb8c15b330ec1de9
CofCool <cofcool@126.com>
2019-06-27 15:56:17 +0800

    ResourceExceptionCodeDescriptor 使用 MessageSourceAccessor 替代 LocaleResolver, MessageSource

--- core/src/main/java/net/cofcool/chaos/server/core/i18n/ResourceExceptionCodeDescriptor.java
+++ core/src/main/java/net/cofcool/chaos/server/core/i18n/ResourceExceptionCodeDescriptor.java
@@ -1,11 +1,11 @@
 package net.cofcool.chaos.server.core.i18n;
 
-import java.util.Locale;
 import net.cofcool.chaos.server.common.core.ExceptionCodeDescriptor;
-import net.cofcool.chaos.server.common.util.WebUtils;
 import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.util.Assert;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
 /**
  * ExceptionCodeDescriptor 的实现, 通过 {@link MessageSource} 获取异常描述信息。
@@ -15,35 +15,23 @@ import org.springframework.web.servlet.support.RequestContextUtils;
  * @see org.springframework.web.servlet.LocaleResolver
  * @see SessionLocaleResolver
  */
-public class ResourceExceptionCodeDescriptor implements ExceptionCodeDescriptor {
+public class ResourceExceptionCodeDescriptor implements ExceptionCodeDescriptor,
+    MessageSourceAware {
 
-    private MessageSource messageSource;
+    private MessageSourceAccessor messageSource;
 
-    public ResourceExceptionCodeDescriptor(MessageSource messageSource) {
-        this.messageSource = messageSource;
+
+
+    public ResourceExceptionCodeDescriptor() {
+        this.messageSource = null;
     }
 
     private String resolve(String type) {
-        return messageSource.getMessage(type, null, type, getLocale());
-    }
-
-    protected Locale getLocale() {
-        try {
-            return (Locale) WebUtils.getRequest().getSession().getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME);
-        } catch(Exception e) {
-            try {
-                org.springframework.web.servlet.LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(
-                    WebUtils.getRequest());
-                if (localeResolver != null) {
-                    return localeResolver.resolveLocale(WebUtils.getRequest());
-                }
-            } catch (Exception ignore) {}
+        if (messageSource == null) {
+            throw new IllegalArgumentException("messageSource cannot be null");
         }
-
-        return Locale.getDefault();
+        return messageSource.getMessage(type, null, type);
     }
-
-
 
     @Override
     public String code(String type) {
@@ -54,4 +42,11 @@ public class ResourceExceptionCodeDescriptor implements ExceptionCodeDescriptor 
     public String description(String type) {
         return resolve(type);
     }
+
+    @Override
+    public void setMessageSource(MessageSource messageSource) {
+        Assert.notNull(messageSource, "messageSource cannot be null");
+        this.messageSource = new MessageSourceAccessor(messageSource);
+    }
+
 }
